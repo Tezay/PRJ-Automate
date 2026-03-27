7105e70949bb428c87962230b34dc7680f1dbbf5
Buffaloexe <romaindenoual1808@gmail.com>
2026-03-27 12:11:31 +0100

    progression fonction standardisation

--- src/automaton/standardize.py
+++ src/automaton/standardize.py
@@ -16,7 +16,8 @@ Note :
     - On ne doit appeler cette fonction que si l'automate n'est pas standard.
 """
 
-from automaton.models import Automaton
+from .models import Automaton
+from .properties import is_standard
 
 
 def standardize(af: Automaton) -> Automaton:
@@ -28,4 +29,35 @@ def standardize(af: Automaton) -> Automaton:
     Returns:
         Un nouvel Automaton standardisé (l'original n'est pas modifié).
     """
-    raise NotImplementedError("TODO (Romain) : Implémenter standardize()")
+
+    #création de la copie de l'automate pour ne pas modifier l'original*
+    af_copy = Automaton()
+    af_copy.alphabet = af.alphabet.copy()
+    af_copy.states = af.states.copy()
+    af_copy.initial_states = af.initial_states.copy()
+    af_copy.terminal_states = af.terminal_states.copy()
+    af_copy.transitions = af.transitions.copy()
+
+    if is_standard(af):
+        print("L'automate est déjà standard, aucun changement effectué.")
+        return af
+    
+    else:
+        af_copy.states.append("i")
+
+        for etat_initial in af.initial_states:
+            for (etat,symbole), destinations in af.transitions.items():
+                if etat == etat_initial:
+                    key = ("i", symbole)
+                    if key not in af_copy.transitions:
+                        af_copy.transitions[key] = []
+                        
+                    af_copy.transitions[key] += destinations
+
+            if etat_initial in af.terminal_states:
+                if"i" not in af_copy.terminal_states:
+                    af_copy.terminal_states.append("i")
+
+        af_copy.initial_states = ["i"]
+        
+    return af_copy
